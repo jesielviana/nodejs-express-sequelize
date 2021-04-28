67a4687e09ca19b27986058ecef7d70b52a0dde7
jesielviana <jesielviana@pm.me>
2021-04-28 16:11:01 -0300

    add jwt authentication

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -11,7 +11,7 @@ class Config {
     this.DATABASE_PASSWORD = process.env.DATABASE_PASSWORD
     this.JWT_SECRET = 'HZADgA9ttB$S!dy!hu3Rauvg!L27'
     this.COOKIE_SECURE = true
-    this.PUBLIC_ROUTES = ['/api/', '/api/students', '/api/login']
+    this.PUBLIC_ROUTES = ['/', '/api/users', '/api/auth/login']
     this.CLIENT_URL = 'https://frontend-ifpi-course.netlify.app'
   }
 }
