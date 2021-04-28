41d6d30f754d4165c098898dca67d8b3840f19af
jesielviana <jesielviana@pm.me>
2021-04-28 16:13:25 -0300

    add jwt authentication

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./src/app')
 const config = require('./src/config')
 const { sequelize } = require('./src/models')
 
-sequelize.sync().then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(config.PORT, () => {
     console.log(`App listening at http://localhost:${config.PORT}`)
   })
