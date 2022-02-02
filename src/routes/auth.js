c5d2e40d1ca48627a7e2834ad5cee360b8eef5da
jesielviana <jesielviana@pm.me>
2022-02-02 19:11:23 -0300

    set cookie sameSite

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,7 +10,7 @@ router.post('/signin', async (req, res) => {
   try {
     const { email, password } = req.body
     const { token, userData } = await authService.signin(email, password)
-    res.cookie('token', token, { maxAge: 3600000, httpOnly: true, secure: config.COOKIE_SECURE });
+    res.cookie('token', token, { maxAge: 3600000, httpOnly: true, sameSite: 'None', secure: config.COOKIE_SECURE });
     res.json({ auth: true, user: userData, /** token: token **/ })
   } catch (err) {
     res.status(401).send({ auth: false, token: null, message: err.message })
